15cdfbd59228f811987d714f0081366370ec836f
Maxime Moreillon <m.moreillon@gmail.com>
2025-07-17 07:48:55 +0900

    axios example

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
 <script setup lang="ts">
+import { useAuth } from "@moreillon/vue-oidc";
+import { watch } from "vue";
 import { RouterLink, RouterView } from "vue-router";
+import axios from "axios";
+
+const { user, ready, access_token } = useAuth();
+
+// Example showing how axios auth header can be updated when the token is refreshed
+function setAxiosAuthHeader(accessToken: string) {
+  axios.interceptors.request.clear();
+  axios.interceptors.request.use(
+    (config: any) => {
+      config.headers = {
+        Authorization: `Bearer ${accessToken}`,
+      };
+      return config;
+    },
+    (error: any) => Promise.reject(error)
+  );
+}
+watch(access_token, () => {
+  if (access_token.value) setAxiosAuthHeader(access_token.value);
+});
 </script>
 
 <template>
@@ -10,7 +32,8 @@ import { RouterLink, RouterView } from "vue-router";
     </nav>
   </header>
   <main>
-    <RouterView />
+    <RouterView v-if="ready && user" />
+    <div v-else>Authenticating...</div>
   </main>
 </template>
 
